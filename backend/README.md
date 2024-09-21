503d004f732b9b93a121a37f1dd01c4eb0a61574
Tom0427 <82128211+Tom-game-project@users.noreply.github.com>
2024-09-21 18:21:32 +0900

    [WIP] add accept_header

---
+++ backend/README.md
@@ -0,0 +1,5 @@
+# how to run the backend system
+
+```
+cargo run
+```
--- backend/src/api/proc.rs
+++ backend/src/api/proc.rs
@@ -1,7 +1,20 @@
-use actix_web::{Error, HttpRequest, HttpResponse, Result};
+use std::fmt::format;
 
-pub async fn index(_:HttpRequest) -> Result<HttpResponse, Error>{
-    Ok(HttpResponse::Ok().content_type("text/html; charset=utf-8").body(
+use actix_web::{Error, HttpRequest, HttpResponse, Result,http::header};
+
+
+pub async fn index(req:HttpRequest) -> Result<HttpResponse, Error>{
+    let accept_header = req
+    .headers()
+    .get(header::ACCEPT)
+    .and_then(|value| value.to_str().ok())
+    .unwrap_or("");
+    match accept_header{
+        "text/html" => {
+
+    Ok(HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(
         format!("{}",
 r#"
 <!DOCTYPE html>
@@ -18,4 +31,17 @@ r#"
 "#
     )
     ))
+        }
+    // "application/wasm" => {
+    //     Ok(HttpResponse::Ok()
+    //         .content_type("application/wasm; charset=utf-8")
+    //         .body()
+    //     )
+    // }
+    _ =>{
+        Ok(HttpResponse::Ok()
+            .content_type("text/plain; charset=utf-8")
+            .body(format!("Requested format is not supported. {} is invalid accept_header",accept_header)))
+    }
+    }
 }
